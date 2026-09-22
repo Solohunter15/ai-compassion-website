10b35c055174a8d0e76dcc5282dac53f70008fee
gabinroy <94969757+gabinroy@users.noreply.github.com>
2026-09-22 20:41:27 +0530

    Comment out TimelineSection component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,7 +16,7 @@ export default function Home() {
       <AboutSection />
       <ProducersPage />
       {/* <SpeakerSection /> */}
-      <TimelineSection />
+      {/* <TimelineSection /> */}
       <SponsorSection />
       <PartnerSection />
       {/* <MeetMyTeam /> */}
